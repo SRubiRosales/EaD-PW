646fb5d279c7739402ff4e148d996b1fdd377df4
SRubiRosales <60980248+SRubiRosales@users.noreply.github.com>
2020-05-21 18:18:45 -0500

    Scripts en HTML
    Scripts del módulo y controlador en el mismo HTML que los usa.

--- web/js/controlador.js
+++ web/js/controlador.js
@@ -4,18 +4,3 @@ app.controller("ControladorInput",function($scope){
     $scope.nombres = "Gerardo Gabriel";
     $scope.apellidos = "Gutiérrez Zavala";
 });
-
-app.controller('listController', function($scope){
-    $scope.ventajas = [
-        "Estudias en tu casa, libre de coronavirus...",
-        "Estudias mientras trabajas",
-        "Realiza tus actividades desde cualquier dispositivo",
-        "Puedes ver tus clases mientras realizas otras actividades",
-        "Olvídate de llegar tarde a clases",
-        "Puedes tomar tus clases desde cualquier lugar",
-        "No pierdes tiempo en el tráfico de camino a la escuela o de regreso a casa",
-        "Interactúa constantemente con alumnos y maestros en foros y chats",
-        "Desarrolla nuevas competencias",
-        "Sé más activo y autónomo",
-    ]
-});
