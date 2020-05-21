7d57bcb3080d172ab5b2505ada9f6854196dca36
Fabián Rodas <60993429+SharkJF@users.noreply.github.com>
2020-05-21 15:01:00 -0500

    controlador
    - controlador-angular
    - puntos-2-y-3

---
+++ web/controlador.js
@@ -0,0 +1,7 @@
+angular.module("EaD",[])
+.controller("ControladorInput",function($scope){
+    $scope.comentario = " ";
+    $scope.nombres = "Gerardo Gabriel";
+    $scope.apellidos = "Gutiérrez Zavala";
+});
+
